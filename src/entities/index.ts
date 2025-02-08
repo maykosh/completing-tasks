89e6db2f3b8112de4f32601901ed621439ab5f5a
maykosh <reacttsdev@gmail.com>
2025-02-08 18:37:45 +0500

    fixed imports

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -2,6 +2,6 @@ export * from "./auth/model/auth-slice";
 export * from "./auth/api/authThunk";
 export * from "./todo/api/fetchToDo";
 export * from "./todo/model/todoSlice";
-export { CreateCategory } from "./todo/ui/createCategory";
+export { CreateCategory } from "./todo/ui/CreateCategory";
 export { Delete } from "./todo/ui/Delete";
-export { CreateTask } from "./todo/ui/createTask";
+export { CreateTask } from "./todo/ui/CreateTask";
